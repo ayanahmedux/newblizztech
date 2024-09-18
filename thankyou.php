6b5ca1531335eef3fba4aa5dafaf920849840514
ayanahmedux <abyansiddqui124@gmail.com>
2024-09-18 23:38:01 +0500

    new

---
+++ thankyou.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<?php include_once("includes/head.php"); ?>
+
+<body class="about-us is-preload">
+<?php include_once("testimonial-global.php") ?>
+
+<h2>THANKYOU</h2>
+
+
+<?php include_once("includes/footer.php") ?>
+</body>
+
+</html>
