ca98261e20d1cc6601b26c70caae728bab913669
raheelUser123 <traheel05@gmail.com>
2025-02-25 23:41:07 +0500

    neae

--- testimonial-global.php
+++ testimonial-global.php
@@ -156,3 +156,49 @@
 				</div>
 			</div>
 		</section>
+		<script>
+			$('.slider-nav')
+	.on('init', function(event, slick) {
+		$('.slider-nav .slick-slide.slick-current').addClass('is-active');
+	})
+	.slick({
+		slidesToShow: 7,
+		slidesToScroll: 7,
+		dots: false,
+		focusOnSelect: false,
+		infinite: false,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 5,
+			}
+		}, {
+			breakpoint: 640,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+		   }
+		}, {
+			breakpoint: 420,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+	   }
+		}]
+	});
+
+$('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+	$('.slider-nav').slick('slickGoTo', currentSlide);
+	var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+	$('.slider-nav .slick-slide.is-active').removeClass('is-active');
+	$(currrentNavSlideElem).addClass('is-active');
+});
+
+$('.slider-nav').on('click', '.slick-slide', function(event) {
+	event.preventDefault();
+	var goToSingleSlide = $(this).data('slick-index');
+
+	$('.slider-single').slick('slickGoTo', goToSingleSlide);
+}); 
+		</script>
