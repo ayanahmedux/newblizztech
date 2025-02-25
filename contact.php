3c12b37c1dcaf46ef01e0195d0fd8ac2b0547e2e
raheelUser123 <traheel05@gmail.com>
2025-02-25 23:43:54 +0500

    neae

--- contact.php
+++ contact.php
@@ -114,7 +114,164 @@
 		<!-- LOGO CONTACT FORM END -->
 
 		<!-- TESTIMONIAL START -->
-		<?php include_once("testimonial-global.php") ?>
+	
+<section id="testimonials-sections" style="padding-top: 80px; padding-bottom: 60px;">
+			<div class="heading-text" style="text-align: center;">
+				<h3>Testimonials</h3>
+				<h1>our <b>clients</b> simply love what we do.</h1>
+				<p style="color: #fff;"> Dive into the genuine voices of those who have experienced the transformative
+					power of Blizztech Solutions. Our clients share their <br> journeys and successes,
+					revealing how our unique approach and relentless commitment have redefined
+					their digital landscapes.<br> These testimonials reflect the real impact of our bespoke
+					solutions, capturing the essence of our <br> collaborative spirit and innovative prowess
+					in their own words.</p>
+			</div>
+			<div class="container">
+				<div id="page">
+					<div class="row" style="margin: auto;">
+						<div class="column small-11 small-centered">
+							<div class="slider slider-nav">
+								
+								<div>
+									<h3><span><img src="./images/testimonialimages/newtestimonial2.webp" /></span></h3>
+								</div>
+								<div>
+									<h3><span><img src="./images/testimonialimages/newtestimonial3.webp" /></span></h3>
+								</div>
+								<div>
+									<h3><span><img src="./images/testimonialimages/newtestimonial.webp" /></span></h3>
+								</div>
+								<div>
+									<h3><span><img src="./images/testimonialimages/test1.webp" /></span></h3>
+								</div>
+								<div>
+									<h3><span><img src="./images/testimonialimages/test3.webp" /></span></h3>
+								</div>
+								<div>
+									<h3><span><img src="./images/testimonialimages/test2.webp" /></span></h3>
+								</div>
+								<div>
+									<h3><span><img src="./images/testimonialimages/test9.webp" /></span></h3>
+								</div>
+							</div>
+							<div class="testimonial-quote">
+								<img src="./images/testimonialimages/quote.webp" />
+							</div>
+							<div class="slider slider-single">
+								<div class="testimonial-text">
+									<h3> Jessica M., Creative Director</h3>
+									<p> Blizztech Solutions transformed our outdated website into a modern
+										masterpiece. Their design not only <br> captured our brand’s essence but also
+										significantly improved user engagement. The team’s attention to <br> detail and
+										innovative approach made all the difference.</p>
+										<div class="fivestar-image">
+											<img src="images/testimonialimages/star5.webp" alt="">
+										</div>
+										<div class="trustpilot">
+											<div class="image-trustpilot">
+												<a href="https://www.trustpilot.com/review/blizztechsolutions.com" target="_blank"><img src="./images/trustpilot.webp" alt=""></a>
+											</div>
+										</div>
+								</div>
+
+								<div class="testimonial-text">
+									<h3> Thomas Clark, Marketing Manager</h3>
+									<p> The animations created by Blizztech Solutions brought our product demos to life
+										in a way we never thought <br> possible. Their creativity and technical skill resulted
+										in eye-catching, engaging <br> visuals that have become a key part of our marketing
+										strategy.</p>
+										<div class="fivestar-image">
+											<img src="images/testimonialimages/star5.webp" alt="">
+										</div>
+										<div class="trustpilot">
+											<div class="image-trustpilot">
+												<a href="https://www.trustpilot.com/review/blizztechsolutions.com" target="_blank"><img src="./images/trustpilot.webp" alt=""></a>
+											</div>
+										</div>
+								</div>
+
+								<div class="testimonial-text">
+									<h3> kenneth, Social Media Coordinator</h3>
+									<p> Elevating our social media presence with innovative marketing strategies led to a
+										dramatic increase in <br>engagement and brand awareness across all our channels.
+										The tailored approach and <br>strategic insights were game-changers.</p>
+										<div class="fivestar-image">
+											<img src="images/testimonialimages/star5.webp" alt="">
+										</div>
+										<div class="trustpilot">
+											<div class="image-trustpilot">
+												<a href="https://www.trustpilot.com/review/blizztechsolutions.com" target="_blank"><img src="./images/trustpilot.webp" alt=""></a>
+											</div>
+										</div>
+								</div>
+
+								<div class="testimonial-text">
+									<h3> Michael C., Founder</h3>
+									<p>The branding overhaul has been transformative. Crafting a cohesive and
+										compelling brand narrative <br> made us stand out in a competitive market, with
+										overwhelmingly positive feedback from clients.</p>
+										<div class="fivestar-image">
+											<img src="images/testimonialimages/star5.webp" alt="">
+										</div>
+										<div class="trustpilot">
+											<div class="image-trustpilot">
+												<a href="https://www.trustpilot.com/review/blizztechsolutions.com" target="_blank"><img src="./images/trustpilot.webp" alt=""></a>
+											</div>
+										</div>
+								</div>
+
+								<div class="testimonial-text">
+									<h3> Jordan., Marketing Director</h3>
+									<p>The PPC campaign was a game-changer for our business. Refining ad strategies
+										and maximizing ROI <br> led to better visibility and conversions, proving highly
+										effective.</p>
+										<div class="fivestar-image">
+											<img src="images/testimonialimages/star5.webp" alt="">
+										</div>
+										<div class="trustpilot">
+											<div class="image-trustpilot">
+												<a href="https://www.trustpilot.com/review/blizztechsolutions.com" target="_blank"><img src="./images/trustpilot.webp" alt=""></a>
+											</div>
+										</div>
+								</div>
+
+								<div class="testimonial-text">
+									<h3> Clarissa, Marketing Director</h3>
+									<p> Top-notch content production greatly enhanced our marketing efforts.
+										Compelling <br>and relevant content contributed to a stronger connection with our
+										audience.</p>
+										<div class="fivestar-image">
+											<img src="images/testimonialimages/star5.webp" alt="">
+										</div>
+										<div class="trustpilot">
+											<div class="image-trustpilot">
+												<a href="https://www.trustpilot.com/review/blizztechsolutions.com" target="_blank"><img src="./images/trustpilot.webp" alt=""></a>
+											</div>
+										</div>
+								</div>
+
+								<div class="testimonial-text">
+									<h3>Morgan L., Digital Marketing Director</h3>
+									<p> The targeted SEO efforts have been transformative for our online presence.
+										Through meticulous keyword <br>research and content optimization, we've seen a
+										significant boost in search engine rankings <br>and organic traffic, leading to
+										enhanced visibility and growth.</p>
+										<div class="fivestar-image">
+											<img src="images/testimonialimages/star5.webp" alt="">
+										</div>
+										<div class="trustpilot">
+											<div class="image-trustpilot">
+												<a href="https://www.trustpilot.com/review/blizztechsolutions.com" target="_blank"><img src="./images/trustpilot.webp" alt=""></a>
+											</div>
+										</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
 
 		<!-- FOOTER START -->
