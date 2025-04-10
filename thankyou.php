90020348f39d9d7a2e73db77c3af6e23e7e9fc20
raheelUser123 <traheel05@gmail.com>
2025-04-10 23:30:43 +0500

    pushed b rahel

--- thankyou.php
+++ thankyou.php
@@ -3,13 +3,16 @@
 <?php include_once("includes/head.php"); ?>
 
 <body class="about-us is-preload">
+<?php include_once("includes/header.php") ?>
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T85BXSH6"
             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+    <h1>Thankyou For Submitted the Form!</h1>
+
     <?php include_once("testimonial-global.php") ?>
 
-    <h2>THANKYOU</h2>
+ 
 
 
     <?php include_once("includes/footer.php") ?>
